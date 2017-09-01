0c5aff754b911f76492a85440510ce1b216e4443
s4kr4 <s4kr4m4@gmail.com>
2017-09-01 04:13:17 +0000

    tmp

--- src/GitHubContributions.js
+++ src/GitHubContributions.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default class GitHubContributions {
+export default class GitHubContributions extends Component {
+  constructor() {
+    super()
+
+    this.calendarSVG = null;
+  }
+
+  componentWillMount() {
+    const req = axios.create({
+      headers: {
+        'Accept': 'text/html'
+      }
+    })
+    req.get('https://github.com/users/s4kr4/contributions')
+      .then(res => {
+        console.log(res)
+        this.calendarSVG = res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   render() {
     return(
       <div>
-        test
+        { this.calendarSVG }
       </div>
     )
   }

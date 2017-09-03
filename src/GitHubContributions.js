a4475b5be1eaf74f4fdfad9dab04f3f226df682a
s4kr4 <s4kr4m4@gmail.com>
2017-09-04 02:07:27 +0900

    Implement fetching data

--- src/GitHubContributions.js
+++ src/GitHubContributions.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class GitHubContributions extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
-    this.calendarSVG = null;
+    this.state = {
+      calendarSVG: "",
+      calendarData: [],
+    }
+
+    this.generateURL = this.generateURL.bind(this)
   }
 
   componentWillMount() {
@@ -14,10 +19,19 @@ export default class GitHubContributions extends Component {
         'Accept': 'text/html'
       }
     })
-    req.get('https://github.com/users/s4kr4/contributions')
+
+    const url = this.generateURL(this.props.username)
+
+    req.get(url)
       .then(res => {
-        console.log(res)
-        this.calendarSVG = res.data
+        const parser = new DOMParser()
+        const domString = res.data.replace(/\r?\n/g, '')
+        const dom = parser.parseFromString(domString, 'application/xml')
+
+        this.setState({
+          calendarSVG: domString,
+          calendarData: dom.querySelectorAll('.day'),
+        })
       })
       .catch(err => {
         console.log(err)
@@ -25,10 +39,21 @@ export default class GitHubContributions extends Component {
   }
 
   render() {
+    for (const data of this.state.calendarData) {
+      const date = data.getAttribute('data-date')
+      const count = data.getAttribute('data-count')
+      console.log(date + ': ' + count)
+    }
+
     return(
       <div>
-        { this.calendarSVG }
+        { this.state.calendarSVG }
       </div>
     )
+  }
+
+  generateURL(username) {
+    const url = 'https://github.com/users/' + username + '/contributions'
+    return url
   }
 }

6b1affd36284d949914f19588c404435bec570e7
s4kr4 <s4kr4m4@gmail.com>
2017-09-05 13:09:57 +0900

    Update

--- src/GitHubContributions.js
+++ src/GitHubContributions.js
@@ -24,7 +24,7 @@ export default class GitHubContributions extends Component<Props, State> {
     }
   }
 
-  componentWillMount() {
+  componentDidMount() {
     const req = axios.create({
       headers: {
         'Accept': 'text/html'
@@ -65,6 +65,8 @@ export default class GitHubContributions extends Component<Props, State> {
   }
 
   render() {
+    this.generateGraphData()
+
     return(
       <div className="github-contributions">
       </div>
@@ -80,11 +82,20 @@ export default class GitHubContributions extends Component<Props, State> {
   }
 
   generateGraphData() {
-    for (const data of this.state.calendarData) {
-      const date: moment = moment(data.date)
-      const count: number = data.count
 
-      console.log(date.format());
+    if (this.state.calendarData.length > 0) {
+      const firstDayData: moment = moment(this.state.calendarData[0].date)
+      const firstDay: string = firstDayData.format('YYYY-MM-DD')
+      const firstWeekDay: number = firstDayData.format('e')
+
+      let graphData: Array<Array<{date: string, count:number}>> = []
+      for (const data of this.state.calendarData) {
+        const date: moment = moment(data.date)
+        const count: number = data.count
+
+        const index: number = Math.abs(date.format('e') - firstWeekDay);
+        console.log(date.format('YYYY-MM-DD') + ': ' + index);
+      }
     }
   }
 }

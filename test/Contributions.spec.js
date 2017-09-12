3b1745e0aba5f2c2091c3ef553df5ffdede902fa
s4kr4 <s4kr4m4@gmail.com>
2017-09-12 13:13:15 +0900

    Update test

--- test/Contributions.spec.js
+++ test/Contributions.spec.js
@@ -8,15 +8,28 @@ import Contributions from '../src/Contributions.js'
 describe('<Contributions />', () => {
   it('set props properly', () => {
     const wrapper = mount(<Contributions GitHub="s4kr4" />)
+
     expect(wrapper.props().GitHub).to.equal('s4kr4')
     expect(wrapper.props().Qiita).to.equal('')
     expect(wrapper.props().to).to.equal('')
   })
 
   it('calls componentDidMount', () => {
-    sinon.spy(Contributions.prototype, 'componentDidMount')
+    const componentDidMount = sinon.spy(Contributions.prototype, 'componentDidMount')
+    const getContributionData = sinon.spy(Contributions.prototype, 'getContributionData')
+    const generateGraphData = sinon.spy(Contributions.prototype, 'generateGraphData')
+
     const wrapper = mount(<Contributions GitHub="s4kr4" />)
-    expect(Contributions.prototype.componentDidMount).to.have.property('callCount', 1)
+
+    expect(componentDidMount.callCount).to.equal(1)
+    expect(getContributionData.callCount).to.equal(1)
+
+    setTimeout(function() {
+      expect(generateGraphData.callCount).to.equal(1)
+    }, 1000);
+
     Contributions.prototype.componentDidMount.restore()
+    Contributions.prototype.getContributionData.restore()
+    Contributions.prototype.generateGraphData.restore()
   })
 })
